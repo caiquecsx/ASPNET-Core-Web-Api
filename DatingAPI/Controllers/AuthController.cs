154ad79911244fa1b14629b9ded98876b73e9959
Caique Campos <caique.c.s@outlook.com>
2019-02-12 12:35:28 -0300

    Controller de autenticação de usuarios

---
+++ DatingAPI/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAPI.Data;
+using DatingAPI.Dtos;
+using DatingAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserToRegisterDto user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            user.Username = user.Username.ToLower();
+
+            if (await repo.UserExists(user.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = user.Username
+            };
+
+            var createdUser = await repo.Register(userToCreate, user.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
--- DatingAPI/Startup.cs
+++ DatingAPI/Startup.cs
@@ -32,6 +32,7 @@ namespace DatingAPI
                 option.UseSqlite("Data Source=DatingApp.db"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
